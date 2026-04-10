0473be7a3d1fd323903f0c3ae48dd2c757e5ea1f
Mehedi Hasan <mehedihasanprog@gmail.com>
2026-04-10 19:13:43 +0600

    add

---
+++ lib/set.dart
@@ -0,0 +1,14 @@
+void main(){
+  //🧠 Set কি?
+  // Set হলো এমন একটি collection যেখানে:
+  // Duplicate value রাখা যায় না ❌
+  // Unique value থাকে ✅
+  // Order fixed না (list এর মতো না)
+  Set <String> fruits = {'Mango','Apple','Orange'};
+  print("Initial Sets are $fruits");
+  fruits.add("Jack Fruit");
+  print(fruits);
+  fruits.addAll({'Papaya','Grape'});
+  print("After Add All ; $fruits");
+  print('Total Items ; ${fruits.length}');
+  }
