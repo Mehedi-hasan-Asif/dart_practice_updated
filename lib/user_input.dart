7a4207a4d2376b503cb72c808d542d983d54f966
Mehedi Hasan <mehedihasanprog@gmail.com>
2026-04-15 17:40:44 +0600

    add ini

---
+++ lib/user_input.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(){
+  print("Enter name : ");
+  String? name =stdin.readLineSync();    //String
+  print("The enter Name is $name");
+
+  // Integer =  int? number = int.parse(stdin.readLineSync()!);
+  //Floating =   double.parse(stdin.readLineSync()!);
+}
