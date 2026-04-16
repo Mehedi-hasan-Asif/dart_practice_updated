5845045663745e607f330c34a8abf9542cf79563
Mehedi Hasan <mehedihasanprog@gmail.com>
2026-04-16 18:08:12 +0600

    add ini

---
+++ lib/for_loop.dart
@@ -0,0 +1,11 @@
+void main(){
+  //for(int i=0; i<10; i ++){
+  //     print("Asif");
+  //   }
+  int total = 0;
+  int n = 100;
+  for(int i = 1;i<=n;i++){
+    total = total + i;
+  }
+  print("The Natural number total is $total");
+}
